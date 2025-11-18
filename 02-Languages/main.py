97d29b744e07e3b31ed323150fae76c5fd73a999
Cao Gia Thinh <caogiathinh.contact@gmail.com>
2025-11-18 14:15:58 +0700

    Add 02-Languages

---
+++ 02-Languages/main.py
@@ -0,0 +1,35 @@
+
+
+if __name__ == '__main__':
+    # ho_va_ten = input('Nhap ten: ')
+    # print("Họ tên bạn nhập vào là:", ho_va_ten)
+
+    # so_nguyen = 1993
+    # print("Kieu du lieu = ", type(so_nguyen))
+    #
+    # so_thuc = 1.5
+    # print("Kieu du lieu = ", type(so_thuc))
+
+    # so_a = 2
+    # so_b = 2
+    # so_c = so_a + so_b
+    #
+    # print(so_c)
+    #
+    # thi_truot = True
+    # khong_thi_truoc = not thi_truot;
+    # print(khong_thi_truoc)
+    #
+    # if (thi_truot):
+    #     print("Cố lên ban oi")
+    # else:
+    #     print("Chúc mừng bạn")
+
+    s = "I love Python"
+    print(s)
+
+    do_dai = len(s)
+    print("Độ dài chuỗi = ", do_dai)
+
+
+
